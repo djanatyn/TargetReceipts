3a1a54144a352c6f61e728ba81744b55454a3d0f
Jonathan Strickland <djanatyn@gmail.com>
2018-08-06 11:16:54 -0400

    use target API to fetch item information

--- TargetReceipts/src/TargetReceipts.hs
+++ TargetReceipts/src/TargetReceipts.hs
@@ -1,46 +1,55 @@
-{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
 
 module TargetReceipts where
 
-import Data.Yaml
-import GHC.Generics
+import           Data.Yaml
+import           GHC.Generics
 
-import Control.Applicative
-import Control.Monad
+import           Control.Applicative
+import           Control.Monad
 
-import qualified Data.Text.IO as TIO
+import qualified Data.Text.IO            as TIO
 
-import Text.Pandoc (def, runIO, handleError)
-import Text.Pandoc.Builder
-import Text.Pandoc.Writers
-import Text.Printf
+import           Text.Pandoc             (def, handleError, runIO)
+import           Text.Pandoc.Builder
+import           Text.Pandoc.Writers
+import           Text.Printf
 
-import Network.HTTP.Client
-import Network.HTTP.Client.TLS
-import Network.HTTP.Simple
+import           Network.HTTP.Client
+import           Network.HTTP.Client.TLS
+import           Network.HTTP.Simple
 
-import Data.Aeson
-import Data.Aeson.Types
+import           Data.Aeson
+import           Data.Aeson.Types
 
-import System.Environment (getArgs)
+import           System.Environment      (getArgs)
 
 type DPCI = String
 type USD = Double
+
 data Item = Item
-  { name :: String
+  { name  :: String
   , price :: USD
-  } deriving (Generic, Show)
+  , url   :: String
+  } deriving Show
 
-instance ToJSON Item
+data Purchase = Purchase
+  { dpci     :: String
+  , discount :: Maybe USD
+  } deriving Show
 
-instance FromJSON Item
+instance FromJSON Purchase where
+  parseJSON = withObject "purchase" $ \o -> do
+    dpci     <- o .: "dpci"
+    discount <- o .:? "discount"
+    return Purchase{..}
 
 
 searchDPCI :: DPCI -> IO Object
 searchDPCI filter = do
   manager <- newManager tlsManagerSettings
-  
+
   parseRequest url >>= httpJSON >>= (return . getResponseBody) where
     url = "https://redsky.target.com/v1/plp/search/?count=1&keyword=" ++ filter
 
@@ -51,10 +60,22 @@ parseResponse :: Object -> Result [(String, USD, String)]
 parseResponse = parse $ (.: "search_response") >=> (.: "items") >=> (.: "Item") >=> (mapM parseItem) where
   parseItem item = do
     title <- item .: "title"
-    price <- item .: "list_price" >>= (.: "price")
+    price <- item .: "list_price" >>= (.: "max_price")
     url   <- item .: "url"
-    
+
     return (title, price, url)
+
+fetchItem :: Purchase -> IO Item
+fetchItem purchase = do
+  response <- searchDPCI $ dpci purchase
+  
+  let item = case parseResponse response of
+              Success [(name, price, url)] -> case discount purchase of
+                Nothing -> Item{..}
+                Just offset -> Item{..} { price = price + offset }
+              _ -> error $ "could not fetch item (" ++ show purchase ++ ")"
+      validURL = ("https://www.target.com" ++ url item) in
+        return $ item { url = validURL }
 
 -- calculate total price incrementally with fold
 totalCost :: [Item] -> [USD]
@@ -64,24 +85,25 @@ priceTable :: [Item] -> Pandoc
 priceTable items = doc $ simpleTable header rows
   where createRow :: Item -> USD -> [Blocks]
         createRow item totalSum = [ plain $ str $ name item
+                                  , plain $ str $ url item
                                   , plain $ str $ formatCost $ price item
-                                  , plain $ str $ formatCost totalSum ] 
-        header = [plain "Item", plain "Cost", plain "Sum"]
+                                  , plain $ str $ formatCost totalSum ]
+        header = [plain "Item", plain "URL", plain "Cost", plain "Sum"]
         rows = zipWith createRow items (tail $ totalCost items)
 
 -- pandoc table rendering
 formatCost :: USD -> String
 formatCost = printf "$%0.2f"
 
-loadItems :: FilePath -> IO [Item]
-loadItems path = either (error . show) id <$> decodeFileEither path
+loadPurchases :: FilePath -> IO [Purchase]
+loadPurchases path = either (error . show) id <$> decodeFileEither path
 
 main :: IO ()
 main = do
   args <- getArgs
   let path = unwords args
 
-  items <- loadItems path
-  result <- runIO (writeRST def (priceTable items)) >>= handleError
-
+  items <- loadPurchases path >>= mapM fetchItem
+  
+  result <- runIO (writeOrg def (priceTable items)) >>= handleError
   TIO.putStrLn result
